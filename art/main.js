5430b7818a35b66397163f6bec36316bef72c589
Mohammad Talha <mtalha210@hotmail.com>
2019-05-15 17:03:22 -0400

    art

---
+++ art/main.js
@@ -0,0 +1,18 @@
+// let art = url("http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg")
+class Classic {
+  constructor(art_url) {
+    this.frame = black-border;
+    this.art_url ? art_url : 'default_url'
+  }
+  render() {
+    let el = document.createELement('div')
+    el.style.width = '100px'
+    el.style.height = '100px'
+    el.style.backgroundColor = 'black'
+
+    // ternary operator
+
+  }
+}
+
+ }
--- java/main.js
+++ java/main.js
@@ -3,10 +3,18 @@ class User {
     this.email = email
     this.password = password
     this.inbox = []
+    this.blocklist = []
   }
+
+
   sendMessage(rec) {
     let msg = new Message()
-    rec.inbox.push(msg)
+
+    if(!rec.blocklist.includes(this.email)){
+      rec.inbox.push(msg)
+      
+    }
+
   }
 }
 
@@ -19,5 +27,31 @@ class Message {
 
 user1 = new User('talha@example.com','111111')
 user2 = new User('aya@example.com','111111')
+user3 = new User('toy@example.com', '111111')
+// msg = new Message('Why do the knicks suck?')
 
-msg = new Message('Why do the knicks suck?')
+let user1form = document.getElementById('user1-form')
+let user2inbox = document.getElementById('user2-user2inbox')
+window.addEventListener('keydown', function(event) {
+  console.log(event.key)
+  if(event.key == 'Shift' && event.key == 'Enter')
+  text.value += '\n'
+  if (event.which == 13) {
+    sendMessage()
+  }
+})
+
+function sendMessage() {
+  let text = document.getElementById('msg-content')
+  let p = document.createElement('p')
+  p.innerText = text.value
+  user2inbox.appendChild(p)
+  text.focus()
+  text.value = ''
+  // alert(text.value)
+}
+
+user1form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  sendMessage()
+})
